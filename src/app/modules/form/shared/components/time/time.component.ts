3869037a69453963cd3c5c4b7134b8579cc8e67f
Caio Macedo <caiomacedo0102@gmail.com>
2023-01-13 15:58:32 -0300

    Atualização tela de Filtro

--- src/app/modules/form/shared/components/time/time.component.ts
+++ src/app/modules/form/shared/components/time/time.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-import { MinuteInterval } from '../../constants/minute-interval.enum';
 
 @Component({
 	selector: 'form-time',
@@ -9,31 +8,36 @@ import { MinuteInterval } from '../../constants/minute-interval.enum';
 export class TimeComponent implements OnInit {
 
 	@Input() label: string = 'Label';
-	@Input() minutesInterval: MinuteInterval = MinuteInterval.ONE;
+	@Input() minutesInterval: 1 | 5 | 10 | 15 | 30 = 1;
+
+	@Input() hourFormat: 'H12' | 'H24' = 'H24';
 
 	@Output() onValueChange: EventEmitter<string> = new EventEmitter<string>();
 
-	hours: Array<number> = Array.from(Array(24).keys());
-	minutes: Array<number> = [];
+	protected hours: Array<string> = new Array<string>();
+	protected minutes: Array<string> = new Array<string>();
+	protected periods: Array<string> = new Array<string>('AM', 'PM');
 
-	protected value: string = '--:--';
 	protected isHidden: boolean = true;
-
-	protected displayHours: Array<number> = [];
-
-	selectedHour: number;
-	selectedMinute: number;
+	protected value: string = '--:--';
 
 	ngOnInit(): void {
-		this.displayHours = this.hours.slice(0, 5);
+		this.initializeHours();
 		this.initializeMinutes();
 	}
 
-	private initializeMinutes() {
-		const its: number = 60 / this.minutesInterval;
-		for (let i = 0; i < its; i++) {
-			this.minutes.push(this.minutesInterval * i);
+	private initializeHours() {
+		this.hours = Array.from(Array(24).keys()).map(h => h.toString().padStart(2, '0'));
+		if (this.hourFormat === 'H12') {
+			this.hours = this.hours.slice(1, 13);
+			this.value.concat(' AM');
 		}
+	}
+
+	private initializeMinutes() {
+		this.minutes = Array.from(Array(60).keys())
+			.filter(x => x % this.minutesInterval == 0)
+			.map(h => h.toString().padStart(2, '0'));
 	}
 
 	protected openClose(): void {
@@ -44,15 +48,20 @@ export class TimeComponent implements OnInit {
 		this.isHidden = true;
 	}
 
-	private updateValue() {
-		let hh = this.selectedHour >= 0 ? this.selectedHour.toString().padStart(2, '0') : '--';
-		let mm = this.selectedMinute >= 0 ? this.selectedMinute.toString().padStart(2, '0') : '--';
-		this.value = `${hh}:${mm}`
-	}
+	protected updateValue(event: Event, time: string): void {
+		const values = this.value.split(/[: ]/g);
+		if(time.toLowerCase() === 'h') {
+			values[0] = String(event);
+		} else if(time.toLowerCase() === 'm') {
+			values[1] = String(event);
+		} else if (time.toLowerCase() === 'p') {
+			values[2] = String(event);
+		}
 
-	private tryEmitValue(): void {
-		if (this.selectedHour >= 0 && this.selectedMinute >= 0) {
-			this.onValueChange.emit(this.value);
+		this.value = `${values[0]}:${values[1]}`;
+
+		if(this.hourFormat === 'H12') {
+			this.value.concat(` ${values[2]}`);
 		}
 	}
 }
